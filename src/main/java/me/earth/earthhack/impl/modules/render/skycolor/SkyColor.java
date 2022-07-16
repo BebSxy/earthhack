54c4b4ae84cae4cf11b8c546d7cd743131ec9eb9
iJese-New <imijese@gmail.com>
2022-07-16 14:55:25 -0500

    what.

--- src/main/java/me/earth/earthhack/impl/modules/render/skycolor/SkyColor.java
+++ src/main/java/me/earth/earthhack/impl/modules/render/skycolor/SkyColor.java
@@ -5,6 +5,7 @@ import me.earth.earthhack.api.module.util.Category;
 import me.earth.earthhack.api.setting.Setting;
 import me.earth.earthhack.api.setting.settings.ColorSetting;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.awt.*;
@@ -13,7 +14,7 @@ public class SkyColor extends Module {
     public SkyColor() {
         super("SkyColor", Category.Render);
     }
-    protected final Setting<Color> color = register(new ColorSetting("Color", new Color(82, 2, 241, 255)));
+    protected final Setting<Color> color = register(new ColorSetting("Color", new Color(178, 115, 255, 255)));
 
     public Color getColor()
     {
@@ -21,9 +22,19 @@ public class SkyColor extends Module {
     }
 
     @SubscribeEvent
-    public void onEntityRender(final EntityViewRenderEvent.FogColors event) {
-        event.setRed(getColor().getRed() / 255.0f);
-        event.setGreen(getColor().getGreen() / 255.0f);
-        event.setBlue(getColor().getBlue() / 255.0f);
+    public void onUpdate(EntityViewRenderEvent.FogColors event) {
+        event.setRed(this.color.getValue().getRed() / 255.0f);
+        event.setGreen(this.color.getValue().getGreen() / 255.0f);
+        event.setBlue(this.color.getValue().getBlue() / 255.0f);
+    }
+
+    @Override
+    public void onEnable() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override
+    public void onDisable() {
+        MinecraftForge.EVENT_BUS.unregister(this);
     }
 }
